e85663c30954b926d8b9183c7cb41eae3755f562
Md Asaduzaman <asadasad9282@gmail.com>
2024-04-30 11:06:05 +0600

    home page layout

--- app/page.js
+++ app/page.js
@@ -1,14 +1,12 @@
-
+import HomeLayout from "./(homeLayout)/layout";
 import Navbar from "./components/shared/_navbar/Navbar";
 import Footer from "./components/shared/footer/Footer";
 import Index from "./pages/home/page";
 
 export default function Home() {
-    return (
-        <main>
-            <Navbar />
-            <Index />
-            <Footer />
-        </main>
-    );
+  return (
+    <HomeLayout>
+      <Index />
+    </HomeLayout>
+  );
 }
