d441ed69f7c8d2c7cc83ba9376f8603495d87436
Remon Tripura <remontripura045@gmail.com>
2024-05-01 09:49:38 +0600

    push

--- app/(home)/page.jsx
+++ app/(home)/page.jsx
@@ -1,7 +1,3 @@
-import HomeLayout from "../(dashboardLayout)/layout";
-import navbar from "../components/shared/navbar/Navbar";
-import Footer from "../components/shared/footer/Footer";
-import RootLayout from "./layout";
 import HomepageContent from "../pages/home/HomepageContent";
 
 export default function Home() {
